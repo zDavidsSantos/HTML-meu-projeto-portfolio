ef48bc2033ab8b17ae26eb747947e4083bfe78bb
zDavidsSantos <zmaximus.liga@gmail.com>
2024-04-06 22:09:40 -0300

    adicionado-js

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+// conmentário de linha dentro do js funciona desta forma
+
+/*- Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html 
+  Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele 
+  Passo 2 - identificar o clique no botão 
+  Passo 3 - adicionar a classe ativo nos projetos escondidos 
+
+- Objetivo 2 - esconder o botão de mostrar mais 
+  Passo 1 - pegar o botão e esconder ele */
+
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)'); 
+
+botaoMostrarProjetos.addEventListener('click',() => {
+    //Passo 3 - adicionar a classe ativo nos projetos escondidos 
+    projetosInativos.forEach(projetoInativo =>{
+        projetoInativo.classList.add('ativo');
+    });
+
+    //Passo 2 - identificar o clique no botão
+    //Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele 
+    botaoMostrarProjetos.add("remover");
+});
